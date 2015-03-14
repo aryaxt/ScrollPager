608ee429ed305d5749603793360b7683cca9d5dc
Aryan Ghassemi <aryaxt@gmail.com>
2015-03-14 08:26:52 -0700

    version 0.1

--- ScrollPager/Source/ScrollPager.swift
+++ ScrollPager/Source/ScrollPager.swift
@@ -5,6 +5,26 @@
 //  Created by Aryan Ghassemi on 2/22/15.
 //  Copyright (c) 2015 Aryan Ghassemi. All rights reserved.
 //
+//
+//	https://github.com/aryaxt/ScrollPager
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//	of this software and associated documentation files (the "Software"), to deal
+//	in the Software without restriction, including without limitation the rights
+//	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//	copies of the Software, and to permit persons to whom the Software is
+//	furnished to do so, subject to the following conditions:
+//
+//	The above copyright notice and this permission notice shall be included in
+//	all copies or substantial portions of the Software.
+//
+//	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//	THE SOFTWARE.
 
 import UIKit
 
@@ -82,6 +102,8 @@ import UIKit
 			self.layer.borderWidth = borderWidth
 		}
 	}
+	
+	@IBInspectable public var animationDuration: CGFloat = 0.2
 	
 	// MARK: - Initializarion -
 	
@@ -186,7 +208,7 @@ import UIKit
 	private func moveToIndex(index: Int, animated: Bool, moveScrollView: Bool) {
 		animationInProgress = true
 		
-		UIView.animateWithDuration(0.2, delay: 0.0, options: .CurveEaseOut, animations: { [weak self] in
+		UIView.animateWithDuration(NSTimeInterval(animationDuration), delay: 0.0, options: .CurveEaseOut, animations: { [weak self] in
 			
 			let width = self!.frame.size.width / CGFloat(self!.buttons.count)
 			let button = self!.buttons[index]
